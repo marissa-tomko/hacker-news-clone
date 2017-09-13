2568e6fb603f31e7e6bb4532fc8015fcd24e60bd
[01;31m[K"name"[m[K: "Marissa Tomko <[01;31m[K"email"[m[K: null>
2017-09-12 19:52:35 -0700

    AJAXIFY

--- app/controllers/post_votes_controller.rb
+++ app/controllers/post_votes_controller.rb
@@ -1,7 +1,13 @@
 post '/posts/:post_id/post_votes' do
   @post = Post.find(params[:post_id])
-  # for this post, find all of its votes and then make a new one
+  # next line says for this post, find all of its votes and then make a new one
   @post_vote = @post.post_votes.create(params[:post_vote])
+  p params
+  p @post_vote
 
-  redirect "/posts/#{@post.id}"
+  if request.xhr?
+    @post.total_votes.to_s
+  else
+    redirect "/posts/#{@post.id}"
+  end
 end
--- public/js/application.js
+++ public/js/application.js
@@ -1,7 +1,35 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
-
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  voteFormListener();
 });
+
+var voteFormListener = function() {
+  $("#vote-form").on("submit", ".vote-button", function(event) {
+    event.preventDefault();
+
+    var $form = $(this);
+    var url = $form.attr("action");
+    var method = $form.attr("method");
+
+    $(".vote-button").hide();
+
+    var call = $.ajax({
+      url: url,
+      method: method,
+      data: $form.serialize()
+    })
+
+    call.done(function(response) {
+      $("#total-votes").text(response);
+    })
+
+    call.fail(function(response) {
+      console.log("FAIL");
+    })
+
+
+
+
+  })
+
+
+}
